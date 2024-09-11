8db89eff99b7015faeb74b5e904e2ced93f2b134
vitoriaif <catzs.2022112linf0316@aluno.ifpi.edu.br>
2024-09-11 09:32:46 -0300

    NH

--- 22/script.js
+++ 22/script.js
@@ -11,3 +11,17 @@ function escreverNumeros() {
         div.innerHTML = "Por favor, insira um número válido!";
     }
 }
+function mostrarModal() {
+   
+    let texto = prompt("Digite o texto para o modal:");
+
+   
+    if (texto) {
+        document.getElementById("modalBody").innerText = texto;
+
+       
+        $('#meuModal').modal('show');
+    } else {
+        alert("Texto não inserido.");
+    }
+}
