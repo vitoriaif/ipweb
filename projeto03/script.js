18e9be7a49ee583811f8b20f157cd178d479bee6
vitoriaif <catzs.2022112linf0316@aluno.ifpi.edu.br>
2024-09-11 10:22:43 -0300

    i

---
+++ projeto03/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#consultar").click(function () {
+      var cep = $("#cep").val();
+      var url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+      $.getJSON(url, function (retorno) {
+        $("#logradouro").val(retorno.logradouro);
+        $("#bairro").val(retorno.bairro);
+        $("#cidade").val(retorno.localidade);
+        $("#uf").val(retorno.uf);
+      });
+    });
+
+    $("#limpar").click(function () {
+      $("#cep").val("");
+      $("#logradouro").val("");
+      $("#bairro").val("");
+      $("#cidade").val("");
+      $("#uf").val("");
+    });
+  });
