eb7cb71106818255290fd792b9d9d56be9bc2e33
vitoriaif <catzs.2022112linf0316@aluno.ifpi.edu.br>
2024-09-11 10:55:57 -0300

    h

--- projeto03/script.js
+++ projeto03/script.js
@@ -1,23 +1,28 @@
 $(document).ready(function () {
     $("#consultar").click(function () {
-      var cep = $("#cep").val();
-      var url = "https://viacep.com.br/ws/" + cep + "/json/";
-
-      $.getJSON(url, function (retorno) {
-        $("#logradouro").val(retorno.logradouro);
-        $("#bairro").val(retorno.bairro);
-        $("#cidade").val(retorno.localidade);
-        $("#uf").val(retorno.uf);
-        $("#ibge").val(retorno.ibge);
-      });
+        var cep = $("#cep").val();
+        if (cep.length === 8) { 
+            var url = "https://viacep.com.br/ws/" + cep + "/json/";
+            $.getJSON(url, function (retorno) {
+                $("#logradouro").val(retorno.logradouro);
+                $("#bairro").val(retorno.bairro);
+                $("#cidade").val(retorno.localidade);
+                $("#uf").val(retorno.uf);
+                $("#ibge").val(retorno.ibge);
+            }).fail(function() {
+                alert("Erro ao consultar o CEP. Verifique o CEP e tente novamente.");
+            });
+        } else {
+            alert("Por favor, insira um CEP válido com 8 dígitos.");
+        }
     });
 
     $("#limpar").click(function () {
-      $("#cep").val("");
-      $("#logradouro").val("");
-      $("#bairro").val("");
-      $("#cidade").val("");
-      $("#uf").val("");
-      $("#ibge").val("");
+        $("#cep").val("");
+        $("#logradouro").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+        $("#ibge").val("");
     });
-  });
+});
