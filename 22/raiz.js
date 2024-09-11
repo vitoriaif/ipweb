ec7e4c2e3827983d0829d581565cf4d596104731
vitoriaif <catzs.2022112linf0316@aluno.ifpi.edu.br>
2024-09-11 09:43:59 -0300

    O

--- 22/raiz.js
+++
@@ -1,4 +0,0 @@
-function calcularRaiz(numero) {
-    let raiz = Math.sqrt(numero); 
-    alert(`A raiz quadrada de ${numero} é ${raiz}`);
-}
--- 22/script.js
+++ 22/script.js
@@ -11,15 +11,24 @@ function escreverNumeros() {
         div.innerHTML = "Por favor, insira um número válido!";
     }
 }
+
+
+function calcularRaiz() {
+    let numero = parseFloat(prompt("Digite um número para calcular a raiz quadrada:"));
+    
+    if (!isNaN(numero) && numero >= 0) {
+        let raiz = Math.sqrt(numero);
+        alert(`A raiz quadrada de ${numero} é ${raiz}`);
+    } else {
+        alert("Por favor, insira um número válido e não negativo.");
+    }
+}
+
 function mostrarModal() {
-   
     let texto = prompt("Digite o texto para o modal:");
 
-   
     if (texto) {
         document.getElementById("modalBody").innerText = texto;
-
-       
         $('#meuModal').modal('show');
     } else {
         alert("Texto não inserido.");
